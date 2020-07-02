d2bec1092d54cb36990aa9f351b8ffc282b1b841
hanks-su <42451381+hanks-su@users.noreply.github.com>
2020-07-02 13:08:40 -0700

    Update vacuumsimulation.py

--- lab1/vacuumsimulation.py
+++ lab1/vacuumsimulation.py
@@ -8,6 +8,7 @@ from lab1.v4agent import V4Agent
 from lab1.v5agent import V5Agent
 from lab1.v6agent import V6Agent
 
+from random import randint
 
 from lab1.liuvacuum import ACTION_NOP
 
@@ -18,11 +19,11 @@ def log_null(msg):
     pass
 
 class VacuumSimulation:
-    def __init__(self, agent, dirt_bias, dirt_distribution, wall_bias, world_seed):
+    def __init__(self, agent, dirt_bias, wall_bias, world_seed):
         self.env = LIUVacuumEnvironment(env_x=20, 
                                         env_y=20, 
-                                        dirt_bias=dirt_bias, 
-                                        dirt_distribution=dirt_distribution, 
+                                        dirt_bias=dirt_bias,
+                                        dirt_distribution = 0.0,
                                         wall_bias=wall_bias, 
                                         world_seed=world_seed)
 
@@ -42,26 +43,39 @@ class VacuumSimulation:
         return self.agent.score()
 
 #########################
-### This runs the same simulation on six different agents.
+# Iterate many times:
+#   Create a 
 
-agents = [V1Agent(20, 20, log_null, 300), 
-          V2Agent(20, 20, log_null, 300),
+def run_agents_in_environment(agents, 
+                              dirt_density, 
+                              wall_density, 
+                              num_samples = 100, 
+                              output_file_name = 'simulation_results.csv',
+                              print_results_to_console = False):
+    with open(output_file_name, 'w') as output_file:
+        for i in range(1, num_samples):
+            seed = randint(1,10000)
+            for a in agents:
+                v = VacuumSimulation(a, dirt_density, wall_density, seed)
+                v.run()
+                output_file.write(f"{a.version},{dirt_density},{wall_density},{a.score()}\n")
+                if print_results_to_console:
+                    print(f"{a.version},{dirt_density},{wall_density},{a.score()}")
+
+
+#######################################
+                    
+agents = [#V1Agent(20, 20, log_null, 300), 
+          #V2Agent(20, 20, log_null, 300),
           V3Agent(20, 20, log_null, 300),
           V4Agent(20, 20, log_null, 300),
-          V5Agent(20, 20, log_null, 300),
-          V6Agent(20, 20, log_null, 300),
+          #V5Agent(20, 20, log_null, 300),
+          #V6Agent(20, 20, log_null, 300),
           ]
 
 
-dirt_density = 0.2
-dirt_uniformity = 0
-wall_density = 0.1
-random_seed = 12321
+dirt_density = 0.3
+wall_density = 0.3
+output_file_name = 'simulation_results.csv'
 
-output_file = 'simulation_results.csv'
-with open(output_file, 'w') as of:
-    for a in agents:
-        v = VacuumSimulation(a, dirt_density, dirt_uniformity, wall_density, random_seed)
-        v.run()
-        of.write(f"{a.version},{dirt_density},{dirt_uniformity},{wall_density},{a.score()}\n")
-        print(f"{a.version},{dirt_density},{dirt_uniformity},{wall_density},{a.score()}\n")
+run_agents_in_environment(agents, dirt_density, wall_density, 100, output_file_name, True)
